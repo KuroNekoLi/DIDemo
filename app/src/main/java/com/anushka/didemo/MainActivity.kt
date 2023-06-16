66631cea7d82e72035ccd50301a7ec1bc462ea99
KuroNekoLi <alice.margatroid.love@gmail.com>
2023-06-16 15:55:27 +0800

    field injection with dagger2

--- app/src/main/java/com/anushka/didemo/MainActivity.kt
+++ app/src/main/java/com/anushka/didemo/MainActivity.kt
@@ -3,17 +3,27 @@ package com.anushka.didemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
-private lateinit var smartPhone : SmartPhone
+@Inject
+ lateinit var smartPhone : SmartPhone
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        DaggerSmartPhoneComponent.create()
-            .getSmartPhone()
-            .makeACallWithRecording()
+        //為什麼我們不使用這種寫法呢？因為我們這樣只能得到SmartPhone的依賴
+//        如果我们想要获取另一个依赖项， 我们就必须在组件接口中创建另一个函数，并在这里调用它。
+//        但是，现在我们不需要为每一个依赖项都创建一个特定的函数。相反，我们可以通过字段注入将这些依赖项带入MainActivity。
+//        DaggerSmartPhoneComponent.create()
+//            .getSmartPhone()
+//            .makeACallWithRecording()
 
+            DaggerSmartPhoneComponent.create()
+                .inject(this)
+        smartPhone.makeACallWithRecording()
+
+        //不使用這個injection方法
 //        val smartPhone = SmartPhone(
 //            Battery(),
 //            SIMCard(ServiceProvider()),
--- app/src/main/java/com/anushka/didemo/SmartPhoneComponent.kt
+++ app/src/main/java/com/anushka/didemo/SmartPhoneComponent.kt
@@ -4,5 +4,13 @@ import dagger.Component
 
 @Component(modules = [MemoryCardModule::class,NCBatteryModule::class])
 interface SmartPhoneComponent {
-    fun getSmartPhone() : SmartPhone
+
+    //一個項目中可能有許多活動和片段，可能有許多依賴項。
+    //如果你有10個像這樣的SmartPhone依賴項，
+    // 你可能需要在組件接口中為所有這些依賴項寫入getter方法，
+    //然後你將需要以這種方式從所有活動中調用它們。
+    //如果你需要在運行時將值傳遞給這些依賴項，這將變得更加困難。
+//    fun getSmartPhone() : SmartPhone
+
+    fun inject(mainActivity: MainActivity)
 }
