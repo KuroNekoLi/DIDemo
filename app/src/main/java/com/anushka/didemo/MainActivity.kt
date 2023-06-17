7eded24786790cd8afc6add2979e5ff747b627bb
KuroNekoLi <127180158+KuroNekoLi@users.noreply.github.com>
2023-06-17 16:58:15 +0800

    the application class

--- app/src/main/java/com/anushka/didemo/MainActivity.kt
+++ app/src/main/java/com/anushka/didemo/MainActivity.kt
@@ -13,6 +13,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        (application as SmartPhoneApplication).smartPhoneComponent
+            .inject(this)
         //為什麼我們不使用這種寫法呢？因為我們這樣只能得到SmartPhone的依賴
 //        如果我们想要获取另一个依赖项， 我们就必须在组件接口中创建另一个函数，并在这里调用它。
 //        但是，现在我们不需要为每一个依赖项都创建一个特定的函数。相反，我们可以通过字段注入将这些依赖项带入MainActivity。
@@ -26,10 +28,10 @@ class MainActivity : AppCompatActivity() {
 //        smartPhone.makeACallWithRecording()
 
         //例如將記憶卡的size設為1000需要這樣
-        DaggerSmartPhoneComponent.builder()
-            .memoryCardModule(MemoryCardModule(1000))
-            .build()
-            .inject(this)
+//        DaggerSmartPhoneComponent.builder()
+//            .memoryCardModule(MemoryCardModule(1000))
+//            .build()
+//            .inject(this)
         //不使用這個injection方法
 //        val smartPhone = SmartPhone(
 //            Battery(),
