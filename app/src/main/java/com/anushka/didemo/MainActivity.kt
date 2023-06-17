1022106298f3ae7265b610d7e9771a18c269e458
KuroNekoLi <127180158+KuroNekoLi@users.noreply.github.com>
2023-06-17 17:07:45 +0800

    單例模式

--- app/src/main/java/com/anushka/didemo/MainActivity.kt
+++ app/src/main/java/com/anushka/didemo/MainActivity.kt
@@ -14,7 +14,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         (application as SmartPhoneApplication).smartPhoneComponent
-            .inject(this)
+            .inject(this) //如果沒有單例模式會每次都創建物件
+
+        smartPhone.makeACallWithRecording()
+
         //為什麼我們不使用這種寫法呢？因為我們這樣只能得到SmartPhone的依賴
 //        如果我们想要获取另一个依赖项， 我们就必须在组件接口中创建另一个函数，并在这里调用它。
 //        但是，现在我们不需要为每一个依赖项都创建一个特定的函数。相反，我们可以通过字段注入将这些依赖项带入MainActivity。
